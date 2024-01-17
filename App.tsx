9d0d53458452f032cbe511576ab65caecb8770b2
DangJW01 <dang.jianwei@1lynx752.com>
2024-01-17 14:16:41 +0800

    Test merge

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Just to test TETSAD</Text>
+      <Text>Just to test TETSADasdasdadwd</Text>
       <StatusBar style="auto" />
       <view></view>
     </View>
